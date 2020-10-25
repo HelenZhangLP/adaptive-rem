238bc0896c9bc3b009de1a182f3faf9a4f9948b6
lipingzhang_i567 <lipingzhang_ajk@58ganji.com>
2020-10-25 21:25:45 +0800

    fxied

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,7 +1,7 @@
 
 // CommonJS模块规范 只有 node 支持该导出
 const path = require('path');
-var htmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
   entry: {
     app: './app.js',
@@ -12,15 +12,4 @@ module.exports = {
     filename: 'js/[name]-[chunkhash].js',
     publicPath: 'http://gulu.top/' // 上线前配置
   },
-  plugins: [new htmlWebpackPlugin({
-    template: 'index.html',
-//    filename: 'inex-[hash].html', //修改生成的模板名称
-    inject: 'head', // 修改生成的 js 文件插入的位置
-    title: 'webpack.config.js 添加参数，通过 ejs 模板在 index.html 中引用', // 这里的参数要在 html 模板中引用
-    date: new Date(),
-    minify: {
-      'collapseWhitespace': true, // 压缩删除空格
-      'removeComments': true, // 压缩去除注释
-    }
-  })]
 }
